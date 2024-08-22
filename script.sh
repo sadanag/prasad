134393f5f8508d554591d347842d0d328cc1683f
nagaaaaaaaaaa <nagaprasad9048@gmail.com>
2024-08-22 12:40:07 +0530

    my commit

---
+++ script.sh
@@ -0,0 +1,14 @@
+bash
+#!/bin/bash
+
+# Addition
+echo "Addition: $1 + $2 = $(( $1 + $2 ))"
+
+# Subtraction
+echo "Subtraction: $1 - $2 = $(( $1 - $2 ))"
+
+# Multiplication
+echo "Multiplication: $1 * $2 = $(( $1 * $2 ))"
+
+# Division
+echo "Division: $1 / $2 = $(( $1 / $2 ))"
